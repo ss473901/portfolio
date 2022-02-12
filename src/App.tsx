ea02b84b95cbd45157973bea568a5ccce295b8fa
Satoshi Sanada <satoshi.473901.1205@gmail.com>
2022-02-12 14:48:10 +0900

    Add firebase.ts reduces

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+
+import { Home } from "./components/pages/Home";
+import { Auth } from "./components/pages/Auth";
+
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+
+  return <>{user.uid ? <Home /> : <Auth />}</>;
+};
+export default App;
